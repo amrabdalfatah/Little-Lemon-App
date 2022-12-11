364461265cbfade250c63867c6a149f0f6ce9656
amrabdalfatah <amr.abdalfatah.mohamed@gmail.com>
2022-12-11 16:20:43 +0200

    Create A Footer

--- components/LittleLemonHeader.js
+++ components/LittleLemonHeader.js
@@ -4,8 +4,8 @@ import {View, Text} from 'react-native';
 export default function LittleLemonHeader() {
     return (
         <View style={{flex:0.1, backgroundColor: '#F4CE14'}}>
-            <Text style={{padding:16, fontSize: 22, color: 'black'}}>
-                Welcome to Little Lemon
+            <Text style={{padding: 10,textAlign: 'center', fontSize: 30, color: 'black'}}>
+                Little Lemon
             </Text>
         </View>
     );
